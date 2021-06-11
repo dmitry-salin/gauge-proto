6f63514d84bb4d19f03b9684f08b75042e15aa1f
Dmitry Salin <dmitry.salin@gmail.com>
2021-06-12 02:36:05 +0300

    Add generated code for golang

--- go/geneproto.sh
+++ go/geneproto.sh
@@ -2,8 +2,8 @@
 
 PATH=$PATH:$GOPATH/bin protoc -I=.. --go_out=. --go-grpc_out=. ../*.proto
 rm -rf gauge_messages
-mv github.com/getgauge/gauge-proto/go/gauge_messages ./
+mv github.com/dmitry-salin/gauge-proto/go/gauge_messages ./
 rm -rf github.com
 cd gauge_messages
-go mod init github.com/getgauge/gauge-proto/go/gauge_messages
+go mod init github.com/dmitry-salin/gauge-proto/go/gauge_messages
 go build ./...
